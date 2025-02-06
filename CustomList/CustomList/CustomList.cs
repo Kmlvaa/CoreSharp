39c0f9fe09b5cbf92fbbe11850f6739044dbaa9e
Kmlvaa <samiraak@code.edu.az>
2025-02-07 00:38:02 +0400

    Custom methods of List

---
+++ CustomList/CustomList/CustomList.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+
+namespace CustomList
+{
+    public class CustomList<T> : IEnumerable<T>
+    {
+        //public Queue<T> Values;
+        public T[] Values;
+        public int ListLimit;
+
+        public CustomList()
+        {
+            //Values = new Queue<T>();
+            Values = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("Can not add null.");
+            }
+            //Values.Enqueue(item);
+            Array.Resize(ref Values, Values.Length + 1);
+            Values[Values.Length - 1] = item;
+        }
+        public T Find(T item)
+        {
+            for (int i = 0; i < Values.Length; i++)
+            {
+                if (EqualityComparer<T>.Default.Equals(Values[i], item))
+                {
+                    return Values[i];
+                }
+            }
+            return default;
+        }
+        public T[] FindAll(Predicate<T> predicate)
+        {
+            T[] values = [];
+
+            for(int i = 0; i < Values.Length; i++)
+            {
+                if (predicate(Values[i]))
+                {
+                    Array.Resize(ref values, values.Length + 1);
+                    values[values.Length - 1] = Values[i];
+                }
+            }
+
+            return values;
+        }
+        public bool Remove(T item)
+        {
+            T[] array = [];
+            if (item == null) Console.WriteLine("Can not remove null");
+
+            if(EqualityComparer<T>.Default.Equals(Find(item), item))
+            {
+                for(int i = 0; i < Values.Length; i++)
+                {
+                    if(EqualityComparer<T>.Default.Equals(Values[i], item))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        Array.Resize<T>(ref array, array.Length + 1);
+                        array[array.Length - 1] = Values[i];
+                    }
+                }
+                Values = array;
+                return true;
+            }
+            else return false;
+        }
+        public int RemoveAll(Predicate<T> predicate)
+        {
+            int count = 0;
+            T[] values = [];
+
+            foreach(T item in Values)
+            {
+                if (predicate(item))
+                {
+                    count++;
+                }
+                else
+                {
+                    Array.Resize<T>(ref values, values.Length + 1);
+                    values[values.Length - 1] = item;
+                }
+            }
+            Values = values;
+
+            return count;
+        }
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CustomList/CustomList/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace CustomList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            list.Add("hello");
+            list.Add("2kakld");
+            list.Add("3ofof");
+
+            //Console.WriteLine(list.Remove("1"));
+            //Console.WriteLine(list.Find(x => x == "2"));
+
+            //Console.WriteLine(list.RemoveAll(x => x.Contains('o')));
+
+            //foreach(var item in list.FindAll(x => x.Contains('o')))
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //foreach (var item in list)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //List<int> ints = new List<int>();
+            //ints.Add(1);
+            //ints.Add(2);
+            //ints.Add(3);
+            //int x = ints.Find(x => x == 3);
+            //Console.WriteLine(x);
+
+
+            //CUSTOM LIST
+
+
+            CustomList<string> customList = new CustomList<string>();
+            customList.Add("hello");
+            customList.Add("salam");
+            customList.Add("ohayo");
+            customList.Add("hello");
+
+            //Console.WriteLine("------------------------------");
+            //CustomList<int> customList = new CustomList<int>();
+            //customList.Add(1);
+            //customList.Add(2);
+            //customList.Add(3);
+            //customList.Add(2);
+            //Console.WriteLine("------------------------------");
+            //Console.WriteLine("Added items:");
+            //foreach (var item in customList.Values)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine("------------------------------");
+            //Console.WriteLine("Items after remove one:");
+            //Console.WriteLine($"Removed item status: {customList.Remove(0)}");
+            //foreach (var item in customList.Values)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine("------------------------------");
+
+            //Console.WriteLine(customList.Find(2));
+
+            //var removedItems = customList.RemoveAll(x => x == 3);
+
+            //Console.WriteLine($"Count of removed items: {removedItems}");
+
+            //foreach (var item in customList.Values)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine("------------------------------");
+
+            foreach (var item in customList.FindAll(x => x.Contains('a')))
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
